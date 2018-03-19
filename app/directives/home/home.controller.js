41b936b5b380e0ae09edb4c524bded95ed5d94c6
Yongki Jay An <hinza0188@gmail.com>
2018-03-19 12:10:55 -0400

    added semicolon again

--- app/directives/home/home.controller.js
+++ app/directives/home/home.controller.js
@@ -3,4 +3,4 @@ var homeController = angular.module('modernBuzzwordApp');
 // This will likely be empty...
 homeController.controller('HomeController', function($scope) {
   $scope.data = "TEST DATA IN home.controller.js";
-})
+});
