9dbfad0617e667568ceeb8d8860c507976288f51
Yongki Jay An <hinza0188@gmail.com>
2018-03-23 11:59:38 -0400

    added bit of css on words

--- app/directives/categories/catWordList.js
+++ app/directives/categories/catWordList.js
@@ -4,14 +4,14 @@ wordList.directive('catWordList', function () {
     return{
         templateUrl: 'app/directives/categories/catWordList.html',
         controller: function($scope, $http) {
-            console.log('inside function for cat word list');
+            //console.log('inside function for cat word list');
             $http.get('app/data/buzzwords.json')
                 .then(function(response){
                     $scope.wordList = response.data.words;
                     $scope.categories = response.data.categories;
 
-                    console.log($scope.wordList);
-                    console.log($scope.categories);
+                    //console.log($scope.wordList);
+                    //console.log($scope.categories);
                 });
         }
     }
